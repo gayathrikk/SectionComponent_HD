35e9d73264b2434e29ac26ac498f3ff2325ad839
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-06-21 10:36:27 +0530

    HD.java

--- src/test/java/com/test/section_component/HD.java
+++ src/test/java/com/test/section_component/HD.java
@@ -50,7 +50,7 @@ public class HD {
 		
 			DesiredCapabilities dc = DesiredCapabilities.chrome();
 		
-			URL url = new URL("http://172.12.20.99:4444/wd/hub");
+			URL url = new URL("http://172.20.23.92:4444/wd/hub");
 		
 			driver = new RemoteWebDriver(url, dc);
 		
